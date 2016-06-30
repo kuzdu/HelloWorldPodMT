d47b7fff172bcceeb7e7cc552e12156b11651e29
Michael Rothkegel <michael.rothkegel@invers.com>
2016-06-30 15:12:16 +0200

    Initial Commit

---
+++ HelloWorldPodMT/HelloWorldPodMT.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldPodMT.h
+//  HelloWorldPodMT
+//
+//  Created by Michael Rothkegel on 30.06.16.
+//  Copyright © 2016 Michael Rothkegel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloWorldPodMT.
+FOUNDATION_EXPORT double HelloWorldPodMTVersionNumber;
+
+//! Project version string for HelloWorldPodMT.
+FOUNDATION_EXPORT const unsigned char HelloWorldPodMTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldPodMT/PublicHeader.h>
+
+
